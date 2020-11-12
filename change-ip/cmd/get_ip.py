72b223fa9da8ba647eb94b83e7320ee569354e03
sector <sector@coldcore.com>
2020-11-12 17:55:01 +0000

    mobile proxy

--- change-ip/cmd/get_ip.py
+++ change-ip/cmd/get_ip.py
@@ -1,15 +1,16 @@
 from requests import get
 
 ip = get('https://api.ipify.org').text
-print('My public IP address is: {}'.format(ip))
+print('My public IP address is {}'.format(ip))
 
 f=open('output/ips.txt','w+')
 lines = f.read().splitlines()
 last_line = lines[-1] if len(lines) > 0 else ""
 if last_line != ip+"\n":
   f.write(ip+'\n')
+  lines.append(ip)
 f.close()
 
-print('\nIPs')
+print('\nIP history')
 for line in reversed(lines):
   print(line)
