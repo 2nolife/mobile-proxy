9168dc49450736bcda8f04baa3e6c79a07d2a9c1
sector <sector@coldcore.com>
2020-11-13 14:48:53 +0000

    mobile proxy

--- bin/ssh-tunnels.template.sh
+++ bin/ssh-tunnels.template.sh
@@ -19,13 +19,13 @@ port3={port3}
 port4={port4}
 
 echo SSH port
-autossh -M 0 -fC -p $srv1_host -N -R 0.0.0.0:$port1:localhost:22 $srv1_port -o $keep_a -o $keep_c -i $prv_key
+autossh -M 0 -fC -p $srv1_port -N -R 0.0.0.0:$port1:localhost:22 $srv1_host -o $keep_a -o $keep_c -i $prv_key
 
 echo Modem CP port
-autossh -M 0 -fC -p $srv1_host -N -R 0.0.0.0:$port2:localhost:8080 $srv1_port -o $keep_a -o $keep_c -i $prv_key
+autossh -M 0 -fC -p $srv1_port -N -R 0.0.0.0:$port2:localhost:8080 $srv1_host -o $keep_a -o $keep_c -i $prv_key
 
 echo HTTP proxy port
-autossh -M 0 -fC -p $srv1_host -N -R 0.0.0.0.0:$port3:localhost:3128 $srv1_port -o $keep_a -o $keep_c -i $prv_key
+autossh -M 0 -fC -p $srv1_port -N -R 0.0.0.0.0:$port3:localhost:3128 $srv1_host -o $keep_a -o $keep_c -i $prv_key
 
 echo VPN port
-autossh -M 0 -fC -p $srv1_host -N -R 0.0.0.0.0:$port4:localhost:1194 $srv1_port -o $keep_a -o $keep_c -i $prv_key
+autossh -M 0 -fC -p $srv1_port -N -R 0.0.0.0.0:$port4:localhost:1194 $srv1_host -o $keep_a -o $keep_c -i $prv_key
