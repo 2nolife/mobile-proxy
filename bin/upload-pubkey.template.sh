64b120a497c960efd433c0aaf7eaf37644199319
sector <sector@coldcore.com>
2020-11-13 21:25:36 +0000

    mobile proxy

--- bin/upload-pubkey.template.sh
+++ bin/upload-pubkey.template.sh
@@ -6,4 +6,4 @@ srv1_host={srv1_host}
 prv_key=~/mobile-proxy/other/mobileproxy_id_rsa
 pub_key=~/.ssh/id_rsa.pub
 
-scp -P $srv1_port $pub_key $srv_user@$srv1_host:~/my_id_rsa.pub -i $prv_key
+scp -i $prv_key -P $srv1_port $pub_key $srv_user@$srv1_host:~/my_id_rsa.pub
