76caf3225eb8adc788ca9324a2e8cca65f7bf400
sector <sector@coldcore.com>
2020-11-16 17:11:26 +0000

    mobile proxy

--- bin/install.sh
+++ bin/install.sh
@@ -5,7 +5,7 @@ echo This script installs packages and applies config overwriting existing files
 echo It meant to run only once on a clean operating system.
 echo You need to know the exact configuration parameters and the key GPG password.
 echo The remote server must be ready to accept the key and SSH tunnels from this unit.
-echo.
+echo
 
 marker=~/mp-installed
 if [ -f $marker ]; then
@@ -23,35 +23,51 @@ read -p "Press Enter to proceed or ^C to stop"
 
 remote_user=$1
 remote_port=$2
-$password=$3
+password=$3
 
-echo.
+echo
 echo Installing packages
 sudo apt-get -y install curl nano squid openvpn nodejs npm python3 git autossh
 
-echo.
+echo
 echo Downloading project
 cd ~
-git clone https://github.com/2nolife/mobile-proxy.git
+if [ -d mobile-proxy ]; then
+  echo mobile-proxy already exists, updating
+  cd mobile-proxy
+  git pull
+else
+  git clone https://github.com/2nolife/mobile-proxy.git
+fi
 
-echo.
-echo Unpacking project key
-read -p "You will now be asked for the key GPG password, press Enter to proceed"
+echo
+echo Decrypting project key
 cd ~/mobile-proxy/other
-gpg mobileproxy_id_rsa.gpg
+if [ -f mobileproxy_id_rsa ]; then
+  echo mobileproxy_id_rsa already exists
+else
+  read -p "You will now be asked for the key GPG password, press Enter to proceed"
+  gpg mobileproxy_id_rsa.gpg
+fi
 chmod 400 mobileproxy_id_rsa
 
-echo.
+echo
 echo Congiguring SSH
+cd ~
+mkdir -p .ssh
 cd ~/.ssh
-ssh-keygen -f id_rsa -N "" -y
+if [ -f id_rsa ]; then
+  echo id_rsa already exists
+else
+  echo -e 'y\n' | ssh-keygen -f id_rsa -N ""
+fi
 chmod 400 id_rsa
-echo "" > config
+echo > config
 echo "Host *" >> config
 echo "  StrictHostKeyChecking no" >> config
 chmod 400 config
 
-echo.
+echo
 echo Configuring Squid
 cd /etc/squid
 sudo cp -n squid.conf squid.default.conf
@@ -60,13 +76,13 @@ sudo touch passwords
 sudo touch trusted.txt
 sudo touch banned.txt
 
-echo.
+echo
 echo Configuring Control Panel
 cd ~/mobile-proxy/change-ip
 mkdir output
 npm install
 
-echo.
+echo
 echo Configuring VPN
 read -p "You will now be asked for VPN settings, press Enter to proceed"
 cd ~
@@ -76,7 +92,7 @@ udo ./openvpn-install.sh
 rm openvpn-install.sh
 mv *.ovpn ~/mobile-proxy/unit
 
-echo.
+echo
 echo Configuring project
 cd ~/mobile-proxy/bin
 python3 setup.py $remote_user $remote_port
@@ -87,11 +103,11 @@ cd ~/mobile-proxy/unit
 /change-pwd.sh $password
 /change-key.sh
 
-echo.
+echo
 echo Finalising
 cd ~
 touch mp-installed
 
-echo.
+echo
 read -p "All done, press Enter to reboot or ^C to stop"
 sudo reboot
